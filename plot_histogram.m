5c6a82923a0978abb847a8b90fe4508338f3f10f
Taylor G <tsg9292@gmail.com>
2014-12-08 11:36:20 -0700

    updated some stuff

--- plot_histogram.m
+++ plot_histogram.m
@@ -1,9 +1,10 @@
-Xt=csvread('output.csv');
-figure
-h=histogram(Xt,'BinLimits',[0,100]);
-title('Histogram of 1000 trials of Xt');
-xlabel('Xt (# of people in the queue at time T)');
-ylabel('frequency');
-saveas(h,'hist','png')
-close
-exit
+function plot_histogram(lam, mu, n, T)
+	Xt=csvread('output.csv');
+	figure
+	h=histogram(Xt);
+	title(['Histogram of 1000 trials of Xt with \lambda = ' num2str(lam) ' and \mu = ' num2str(mu) '.']);
+	xlabel(['Xt (# of people in the queue at time ' num2str(T) ')']);
+	ylabel('frequency');
+	saveas(h,'hist','png');
+	close
+	exit
--- queue.sh
+++ queue.sh
@@ -5,6 +5,7 @@ if (($# != 4));then
 	echo 'exiting...'
 	exit
 fi
+rm -f hist.png
 python queue_simulator.py $1 $2 $3 $4
-matlab -nodisplay -nosplash -r plot_histogram
+matlab -nodisplay -nosplash -r "plot_histogram('${1}', '${2}', '${3}', '${4}')"
 google-chrome hist.png
